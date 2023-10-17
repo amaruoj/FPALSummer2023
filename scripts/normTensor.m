929b95bb90a4000ebed00e9467d89e31fca1003f
amaruoj <amaru@stanford.edu>
2023-10-17 15:58:06 -0700

    tensor normalization updated

--- scripts/normTensor.m
+++ scripts/normTensor.m
@@ -1,22 +1,25 @@
-function re = normTensor(tensor,U)
+function re = normTensor(folder)
 % input:
-% tensor:   Reynolds Stress tensor without normalization
-% U:    time-averaged streamwise centerline velocity
+% folder: path to outputs
 
 % output:
 % result:   normalized Reynolds Stress tensor
 
+% load data
+raw_re = load(fullfile(folder,'raw_re.mat')).tensor;
+U = load(fullfile(folder,'centerU.mat')).U;
+
 % create normalization
-norm = U.^2;
+norm = U'.^2;
 
 % normalize tensor
-re = tensor ./ norm;
+re = raw_re ./ norm;
 
 % save normalized tensor
 tic
 disp('saving normalized tensor...')
 filename = 'normRe';
-out_dir = fullfile('..','matrices','norm_tensors');
+out_dir = fullfile('..','output_data');
 save(fullfile(out_dir,filename),'re','-v7.3');
 disp(['done! saved as ', filename, '.mat! ♪(´▽｀)'])
 toc
